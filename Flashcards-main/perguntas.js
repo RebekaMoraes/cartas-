51611f3d820b3dc696565e525ec6df002b093487
RebekaMoraes <133163321+RebekaMoraes@users.noreply.github.com>
2024-09-24 12:08:29 -0300

    Add files via upload

---
+++ Flashcards-main/perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'Astronomia',
+    'Quais são os 8 corpos celestes?',
+    'Júpiter, Marte, Mercúrio, Netuno, Saturno, Terra, Urano, Vênus',
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os tipos de Biologia?',
+    'Existem três ramos principais reconhecidos da biologia, que são: botânica, zoologia, microbiologia',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Amazonas?',
+    'Manaus'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz Abacaxi em inglês?',
+    'Pineapple'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se divide a matemática?',
+    'A matemática é subdividida em aritmética, álgebra, trigonometria e geometria'
+)
+
+criaCartao(
+    'Química',
+    'Quem é o criador da química?',
+    'Antoine Lavoisier'
+)
+
+criaCartao(
+    'Física',
+    'O que é p na Física?',
+    'A potência'
+)
+
